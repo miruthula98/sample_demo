a0e80aa5dc5d56473f849223a14fba44b40ff849
miruthula98 <106802987+miruthula98@users.noreply.github.com>
2022-08-13 01:07:52 +0530

    Add files via upload

---
+++ MavenprojectDSL.groovy
@@ -0,0 +1,15 @@
+job('First MAven Job through DSL') {
+description("First maven job generated through a DSL on ${new Date()}, the project is a maven project hosted on github")
+scm {
+    git("git@github.com:miruthula98/sample_demo.git", master)
+}
+triggers {
+    scm(* * * * *)
+}
+steps {
+    maven('clean package', sampleproject/pom.xml)
+}
+publishers {
+    archiveArtifacts '**/*.jar'
+}
+}
